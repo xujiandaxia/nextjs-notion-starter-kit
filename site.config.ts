b845cb8c81e93d11aa59be50c8fd73ff40ec681f
xujiandaxia <xujiandaxia@sina.com>
2022-06-23 23:13:31 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '359700cefe8249ada87a36ccd0b40c83',
+  rootNotionPageId: '7082028111e042998e6b8c6ae81ca8a4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
