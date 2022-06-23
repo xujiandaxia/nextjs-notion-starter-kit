75052ce8c111ccb59097ea9ac124e1aa971d4d98
xujiandaxia <xujiandaxia@sina.com>
2022-06-23 20:55:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e3854422165f44658ea9c208f13a242d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '阿饭的宝藏',
+  domain: 'afanos.xyz',
+  author: '饭饭',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
